f8e6fe92dfedb8803f93d91be8f8d0b09e12fc24
Alind Khare <alindkhare@gatech.edu>
2019-10-16 16:41:18 -0400

    SERVE [Resnet Profiling]

---
+++ python/ray/experimental/serve/examples/replica_examples/resnet_replica_cdf.py
@@ -0,0 +1,116 @@
+import time
+
+import requests
+from werkzeug import urls
+import ray
+from ray.experimental import serve
+from ray.experimental.serve.utils import pformat_color_json
+import json
+from ray.experimental.serve.utils import BytesEncoder
+from torchvision.models.resnet import resnet50
+import io
+from PIL import Image
+from torch.autograd import Variable
+import torchvision.transforms as transforms
+import base64
+from pprint import pprint
+import torch
+
+def query():
+	d = {
+	'index': '',
+	'start_time': '',
+	'end_time': '',
+	'slo': ''
+	    }
+	return d
+
+class Transform:
+	def __init__(self,transform):
+		self.transform = transform
+	def __call__(self,batch_data):
+		batch_size = len(batch_data)
+		result = []
+		for i in range(batch_size):
+			data = Image.open(io.BytesIO(base64.b64decode(batch_data[i])))
+			if data.mode != "RGB":
+				data = data.convert("RGB")
+			data = self.transform(data)
+			# data = data.unsqueeze(0)
+			result.append(data)
+		return result
+
+class Resnet50:
+	def __init__(self, model):
+		self.model = model
+
+	def __call__(self, batch_data):
+		# if 'transform' in context:
+		# data = context['transform']
+		data = torch.stack(batch_data)
+		data = Variable(data)
+		data = data.cuda()
+		outputs = self.model(data)
+		_, predicted = outputs.max(1)
+		return predicted.cpu().numpy().tolist()
+
+
+min_img_size = 224
+transform = transforms.Compose([transforms.Resize(min_img_size),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                              std=[0.229, 0.224, 0.225])])
+model = resnet50(pretrained=True)
+model = model.cuda()
+
+serve.init(object_store_memory=int(1e9),blocking=True)
+#create Backends
+serve.create_backend(Transform, "transform:v1",0,transform)
+serve.create_backend(Resnet50,"r50",1,model)
+
+# create service
+serve.create_no_http_service("transform",max_batch_size=2)
+serve.create_no_http_service("imagenet-classification",max_batch_size=4)
+
+#link service and backend
+serve.link_service("transform", "transform:v1")
+serve.link_service("imagenet-classification", "r50")
+
+serve.add_service_dependencies("pipeline1","transform","imagenet-classification")
+
+# Provision the PIPELINE (You can provision the pipeline only once)
+serve.provision_pipeline("pipeline1")
+
+
+dependency = serve.get_service_dependencies("pipeline1")
+pipeline_handle = serve.get_handle("pipeline1")
+
+
+future_list = []
+query_list = []
+associated_query = {}
+for r in range(12):
+	q = query()
+	q['slo'] = 70
+	q['index'] = r
+	req_json = { "transform": base64.b64encode(open('elephant.jpg', "rb").read()) }
+	req_json['slo'] = q['slo']
+	q['start_time'] = time.time()
+	f = pipeline_handle.remote(**req_json)
+	associated_query[f] = q
+	future_list.append(f)
+# results = ray.get(future_list)
+# for result in results:
+# 	print("-----------------------------")
+# 	print(result)
+left_futures = future_list
+while left_futures:
+	completed_futures , remaining_futures = ray.wait(left_futures,timeout=0.05)
+	if len(completed_futures) > 0:
+		result = ray.get(completed_futures)
+		associated_query[completed_futures[0]]['end_time'] = time.time()
+		print("--------------------------------")
+		print(result)
+	left_futures = remaining_futures
+pprint(associated_query)
+
