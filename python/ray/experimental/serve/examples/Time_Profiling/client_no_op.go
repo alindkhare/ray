8d4a19dc8c5192bc8e94fb9d42fec7506756ab11
Alind Khare <alindkhare@gatech.edu>
2019-10-19 17:41:49 -0400

    SERVE [Example for Time Profiling: No-op client written in go new version]

--- python/ray/experimental/serve/examples/Time_Profiling/client_no_op.go
+++ python/ray/experimental/serve/examples/Time_Profiling/client_no_op.go
@@ -1,20 +1,26 @@
 package main
-import(
-	"github.com/nahid/gohttp"
-	// "fmt"
+import (
+     "fmt"
+     "os"
+     "net/http"
+     "time"
+     "io/ioutil"
 )
-
-
+func MakeRequest(url string, ch chan<-string) {
+  start := time.Now()
+  resp, _ := http.Get(url)
+  secs := time.Since(start).Seconds()
+  body, _ := ioutil.ReadAll(resp.Body)
+  ch <- fmt.Sprintf("%.2f elapsed with response length: %d %s", secs, len(body), url)
+}
 func main() {
-	req := gohttp.NewRequest()
-	ch := make(chan *gohttp.AsyncResponse)
-
-	for i := 0; i <= 100; i++ {
-		req.AsyncGet("http://127.0.0.1:8000/echo", ch)
-	}
-	// for i:=0; i<=100; i++ {
-	// 	op := <-ch
-
-	// 	fmt.Println(op.GetBodyAsString())
-	// }
+  start := time.Now()
+  ch := make(chan string)
+  for i := 0; i <= 100; i++ {
+      go MakeRequest("http://127.0.0.1:8000/echo", ch)
+  }
+  for i := 0; i <= 100; i++ {
+    fmt.Println(<-ch)
+  }
+  fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }
